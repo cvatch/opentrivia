ae5a3327c542ebeac0acd301f1b2a154dcdaf138
Ubuntu <kirkins@gmail.com>
2017-03-22 03:53:49 +0000

    progress on email posting

--- backend/email/email.js
+++ backend/email/email.js
@@ -1,39 +1,44 @@
-// load aws sdk
-var aws = require('aws-sdk');
+var aws = require('aws-sdk')
+aws.config.loadFromPath('config.json')
+const cors = require('cors')
+const ses = new aws.SES({apiVersion: '2010-12-01'})
+const to = ['kirkins@gmail.com']
+const from = 'kirkins@gmail.com'
+const express = require('express')
+const bodyParser = require('body-parser')
 
-// load aws config
-aws.config.loadFromPath('config.json');
+var app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
 
-// load AWS SES
-var ses = new aws.SES({apiVersion: '2010-12-01'});
+var corsOptions = {
+  origin: 'https://cvatch.github.io/opentrivia/',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+};
 
-// send to list
-var to = ['kirkins@gmail.com']
+app.post('/email', cors(corsOptions), function (req, res) {
+  var parsedJson = req.body
+  console.log(parsedJson);
+  ses.sendEmail({ 
+    Source: from, 
+    Destination: { ToAddresses: to },
+    Message: {
+      Subject: {
+        Data: 'Opentrivia Suggested Edit'
+      },
+      Body: {
+        Text: {
+          Data: 'Stop your messing around',
+        }
+      }
+    }
+  }, function(err, data) {
+    if(err) throw err
+    res.status(200).send('{"message": "worked"}')
+    console.log('Email sent:')
+    console.log(data);
+  })
+})
 
-// this must relate to a verified SES account
-var from = 'kirkins@gmail.com'
-
-
-// this sends the email
-// @todo - add HTML version
-ses.sendEmail( 
-	{ 
-		Source: from, 
-		Destination: { ToAddresses: to },
-		Message: {
-			Subject: {
-				Data: 'A Message To You Rudy'
-			},
-			Body: {
-				Text: {
-					Data: 'Stop your messing around',
-				}
-			}
-		}
-	}
-, function(err, data) {
-	if(err) throw err
-	console.log('Email sent:');
-	console.log(data);
-});
-
+app.listen(3999)
