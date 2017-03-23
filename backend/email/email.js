85b4eb2f8fb2243ca0408b0ffebdae5d54d1599c
Ubuntu <kirkins@gmail.com>
2017-03-23 00:43:22 +0000

    update email script, working state

--- backend/email/email.js
+++ backend/email/email.js
@@ -1,41 +1,49 @@
 var aws = require('aws-sdk')
 aws.config.loadFromPath('config.json')
-const cors = require('cors')
 const ses = new aws.SES({apiVersion: '2010-12-01'})
-const to = ['kirkins@gmail.com']
-const from = 'kirkins@gmail.com'
+const to = ['projectrivia+appEdit@gmail.com']
+const from = 'projectrivia@gmail.com'
 const express = require('express')
 const bodyParser = require('body-parser')
 
 var app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(cors())
 
-var corsOptions = {
-  origin: 'https://cvatch.github.io/opentrivia/',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
-};
+app.get('/', function(req, res){
+  res.send('hello world');
+})
 
-app.post('/email', cors(corsOptions), function (req, res) {
+app.post('/send', function (req, res) {
+  console.log(req.body)
   var parsedJson = req.body
-  console.log(parsedJson);
+  var editId = parsedJson.id
+  var editType, editText
+  if(parsedJson.question) {
+    var editText = parsedJson.question
+    editType = "question"
+  }
+  if(parsedJson.answer) {
+    var editText = parsedJson.answer
+    editType = "answer"
+  }
   ses.sendEmail({ 
     Source: from, 
     Destination: { ToAddresses: to },
     Message: {
       Subject: {
-        Data: 'Opentrivia Suggested Edit'
+        Data: 'Opentrivia ' + editType + ' edit - ID #' + editId
       },
       Body: {
         Text: {
-          Data: 'Stop your messing around',
+          Data: 'Suggested edit for ' + editType +' #' + editId + '\n\nSuggested Edit: ' + editText +
+                '\n\nOld Question: ' + parsedJson.oldQuestion + '\n\nOld Answer: ' + parsedJson.oldAnswer,
         }
       }
     }
   }, function(err, data) {
-    if(err) throw err
-    res.status(200).send('{"message": "worked"}')
+    if(err) res.status(400).send('{"message": "Failed to send message."}')
+    res.status(200).send('{"message": "Thank you for contributing to the Open Trivia Project."}')
     console.log('Email sent:')
     console.log(data);
   })
